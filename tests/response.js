1e0e26f786a38694aa9b6440a250493ea7e34669
claycuy <unggaslauncher@gmail.com>
2025-01-05 17:18:53 +0800

    Update system

--- tests/response.js
+++ tests/response.js
@@ -2,5 +2,5 @@ import { ALSModel } from '../model.js'
 
 const model = new ALSModel();
 
-const result = model.response({ user: "guess", prompt: "gimana kabarmu?" });
+const result = model.response({ user: "guess", prompt: "halo" });
 console.log(result.details.response);

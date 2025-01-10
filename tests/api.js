7b7b734cd1fcf64581e85375adfdbb2a5935e3dd
claycuy <unggaslauncher@gmail.com>
2025-01-10 10:26:28 +0800

    -

--- tests/api.js
+++
@@ -1,9 +0,0 @@
-fetch('http://sayals.free.nf/endpoint.php', {
-    method: 'GET',
-    headers: {
-        'API-Key': 'fa1fa81be1d28b6901bf797efd6b44164550dae259b975a32a'
-    }
-})
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
